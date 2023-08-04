c93f39f83d30bc67d994316a93ba429ece3ac1e8
Ishaan1710 <128537896+Ishaan1710@users.noreply.github.com>
2023-08-04 17:12:57 +0530

    Update app.js
    New version of node does not identify '...//localhost:27017/...', 
    Here, we have changed it to '...//127.0.0.1"27017/...'

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 //create mongoose connectuon
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/StudentHelp')
+mongoose.connect('mongodb://127.0.0.1:27017/StudentHelp')
     .then(() => console.log('Connected to MongoDB!'));
 
 //import models
